964d67d7f465f95fadb7cfd637958ebd0f20b1f6
Franky Lau <frankylau96@hotmail.com>
2025-06-07 22:22:06 +0800

    Add additional technical indicators

--- main.py
+++ main.py
@@ -15,13 +15,25 @@ def Main() -> None:
 
     FeatureEngineer = FeatureEngineering()
 
-    IndicatorsToApply = ["SMA", "EMA", "RSI", "MACD", "BollingerBands"]
+    IndicatorsToApply = [
+        "SMA",
+        "EMA",
+        "RSI",
+        "MACD",
+        "BollingerBands",
+        "OnBalanceVolume",
+        "WilliamsPercentR",
+        "CommodityChannelIndex",
+    ]
     IndicatorParameters = {
         "SMA": {"Window": 20},
         "EMA": {"Window": 12},
         "RSI": {"Window": 14},
         "MACD": {"FastPeriod": 12, "SlowPeriod": 26, "SignalPeriod": 9},
         "BollingerBands": {"Window": 20, "StandardDeviations": 2.0},
+        "OnBalanceVolume": {},
+        "WilliamsPercentR": {"Window": 14},
+        "CommodityChannelIndex": {"Window": 20},
     }
 
     Data = FeatureEngineer.ApplyTechnicalAnalysis(
